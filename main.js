c995dd3972907b04f6942b76ad014732824ebff9
Oleksandr Leskiv <sasha26.05.2012@gmail.com>
2019-09-20 18:36:28 +0300

    paths

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let GameStates = {
     MENU: 0,
     HIGHSCORES: 1,
     GAME_LAUNCH: 2,
-    GAME: 3
-}
+    GAME: 3,
+};
 let currentGameState = GameStates.MENU; //we'll be visible to every object
 
 let gameFont;
@@ -13,10 +13,10 @@ let screens = []; //thing to update every frame
 const GAME_WIDTH = 1280, GAME_HEIGHT = 720;
 
 //all images are loaded in constructors - so we'll place these in preload
-function preload() {
+function preload () {
     window.width = GAME_WIDTH;
     window.height = GAME_HEIGHT;
-    gameFont = loadFont("data/fonts/GameOverFont.ttf");
+    gameFont = loadFont('data/fonts/GameOverFont.ttf');
 
     screens.push(new Background());
     screens.push(new Menu());
@@ -28,25 +28,26 @@ function preload() {
     screens.push(createCancelButton());
 }
 
-function setup() {
+function setup () {
     createCanvas(GAME_WIDTH, GAME_HEIGHT);
 }
 
 let cancelButton;
-function createCancelButton() {
-    cancelButton = createButton("<-");
+
+function createCancelButton () {
+    cancelButton = createButton('<-');
     cancelButton.size(BUTTON_SIZE, BUTTON_SIZE);//it's just s square
     cancelButton.position(window.width - BUTTON_SIZE, window.height - BUTTON_SIZE);
-    cancelButton.addClass("button");
+    cancelButton.addClass('button');
     let cancelBtnOpacity = 0;
     cancelButton.draw = function (deltaTime) {
         cancelButton.hidden = currentGameState == GameStates.MENU; //from every button except MENU we can go back
-        cancelBtnOpacity = lerp(cancelBtnOpacity, cancelButton.hidden? 0:1, 2 * deltaTime);
-        cancelButton.style("opacity", cancelBtnOpacity.toString());
-    }
+        cancelBtnOpacity = lerp(cancelBtnOpacity, cancelButton.hidden? 0 : 1, 2*deltaTime);
+        cancelButton.style('opacity', cancelBtnOpacity.toString());
+    };
     cancelButton.mouseClicked(function () {
         if (cancelButton.hidden) return;
-        for (let i=0; i<screens.length; i++) {
+        for (let i = 0; i < screens.length; i++) {
             if (screens[i].onBackPressed) {
                 screens[i].onBackPressed();
             }
@@ -65,10 +66,11 @@ function createCancelButton() {
 
 let t = 0;
 let previousDate = performance.now();
-function draw() {
+
+function draw () {
     //mechanism that counts delta time between frames
     let currentDate = performance.now();
-    let deltaTime = min((currentDate - previousDate) / 1000, 1);
+    let deltaTime = min((currentDate - previousDate)/1000, 1);
     previousDate = currentDate;
 
     t += deltaTime;
@@ -78,44 +80,52 @@ function draw() {
             s.update(deltaTime);
         }
         if (s.draw) {
-            s.draw(deltaTime);
+            try {
+                s.draw(deltaTime);
+            } catch (e) {
+
+            }
         }
     }
 }
 
 /* functions below are called by p5. Some screens may want to
 receive these events to. Forwarding to them here. */
-function mousePressed() {
+function mousePressed () {
     for (let i = 0; i < screens.length; i++) {
         if (screens[i].onMousePressed) {
             screens[i].onMousePressed(mouseX, mouseY);
         }
     }
 }
-function mouseReleased() {
+
+function mouseReleased () {
     for (let i = 0; i < screens.length; i++) {
         if (screens[i].onMouseReleased) {
             screens[i].onMouseReleased(mouseX, mouseY);
         }
     }
 }
-function mouseDragged() {
+
+function mouseDragged () {
     for (let i = 0; i < screens.length; i++) {
         if (screens[i].onMouseDragged) {
             screens[i].onMouseDragged(mouseX, mouseY);
         }
     }
 }
-function mouseWheel(event) {
+
+function mouseWheel (event) {
     for (let i = 0; i < screens.length; i++) {
         if (screens[i].onMouseWheel) {
             screens[i].onMouseWheel(event);
         }
     }
 }
-function keyPressed(event) {
-    switch(event.key) {
-        case "Escape":
+
+function keyPressed (event) {
+    switch (event.key) {
+        case 'Escape':
             cancelButton._events.click();
             break;
     }
